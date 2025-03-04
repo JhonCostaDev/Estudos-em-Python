43a8feda4d930bd27e7fe80e61ae545db5f0701b
Jonathan Costa <111503993+JhonCostaDev@users.noreply.github.com>
2025-03-03 22:19:03 -0300

    add: find_number.py

---
+++ python_basics/challenges/find_number.py
@@ -0,0 +1,4 @@
+# Construa um programa que realiza o sorteio de um número entre 1 e 15.
+# O usuário terá 3 chances de acertar o valor.
+# A cada tentativa você deve informar se o chute é maior que o número sorteado.
+# Caso o usuário acerte, dê o parabéns.
