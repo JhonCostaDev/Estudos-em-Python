0e616caa3e0025d56c5f10811ccfeec9204e6d4c
Jonathan Costa <jhoncosta2@gmail.com>
2025-03-05 17:13:53 -0300

    conclued function check_answer

--- json/read_json.py
+++ json/read_json.py
@@ -9,8 +9,8 @@ def load_and_convert_json_to_dict(json_file) -> dict:
         data = json.load(f)
     return data
 
-def load_question(dictionary, list_position=0) -> dict:
-    key = list(dictionary.keys())[list_position]
+def load_question(dictionary, list_position=0, chapter=0) -> dict:
+    key = list(dictionary.keys())[chapter]
     list_lenght = len(dictionary[key]) # tamanho 
     list_questions = dictionary[key]
 
@@ -28,8 +28,28 @@ def show_question(dictionary):
 
 
 def get_user_choise():
-    user_choise = input('Type your answer\n')
-    #validate answer
+    valid_options = ['A', 'B', 'C', 'D']
+    while True:
+        user_choise = input('Type your answer\n').upper()
+        if user_choise in valid_options:
+            return user_choise
+        print('Invalid option, please choise A, B, C, and D')
+    
+    
+#validate answer
+
+def check_answer(user_answer, selected_question):
+    asnwer = user_answer
+    question = selected_question['options']
+    correct_answer = selected_question['answer']
+    # print(question[asnwer])
+    # print(corect_answer)
+    
+    if question[asnwer] == correct_answer:
+        return True
+    else:
+        return False
+    
 
 
 
@@ -38,10 +58,18 @@ def get_user_choise():
 
 #print(type(load_and_convert_json_to_dict(file_path)))
 #print(load_question(load_and_convert_json_to_dict(file_path)))
-show_question(load_question(load_and_convert_json_to_dict(file_path), 1))
-get_user_choise()
 
+# show_question(load_question(load_and_convert_json_to_dict(file_path), 1))
+#get_user_choise()
+def main():
+    data = load_and_convert_json_to_dict(file_path)
+    question = load_question(data)
+    show_question(question)
+    answer = get_user_choise()
+    print(check_answer(answer, question))
+    
 
+main()
 
 
 
--- python_basics/day_5/functions.py
+++ python_basics/day_5/functions.py
@@ -2,3 +2,22 @@
 a = 2
 a
 # %%
+data = {
+    'pergunta': 'qual a fruta',
+    'opcoes': {
+        'a': 'goiaba',
+        'b': 'maca',
+        'c': 'laranja',
+        'd': 'limao'
+    },
+    'resposta': 'goiaba'
+}
+# %%
+reposta = 'b'
+# %%
+data['pergunta']#.get(reposta) #== data['restosta']
+# %%
+data['resposta']
+# %%
+data['opcoes'].get(reposta) == data['resposta']
+# %%
